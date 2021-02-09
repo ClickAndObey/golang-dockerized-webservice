0c5485c27759ba85122ca08257c2d4fac1f02ac0
drush211 <thedrush21@gmail.com>
2021-02-09 08:49:16 -0600

    don't pass in parameter at startup.

--- docker/app/run_webservice.sh
+++ docker/app/run_webservice.sh
@@ -4,5 +4,5 @@ APP_NAME=webservice
 ENVIRONMENT=${ENVIRONMENT-docker}
 
 echo "Running The Webservice ${APP_NAME} with environment '${ENVIRONMENT}'..."
-"/${APP_NAME}/golang-dockerized-webservice" "/configuration/${ENVIRONMENT}/config.yaml"
+"/${APP_NAME}/golang-dockerized-webservice"
 echo "The Webservice has finished."
